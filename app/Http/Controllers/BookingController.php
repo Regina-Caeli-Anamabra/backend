cb5d9dedf60a0c219d92bb60e5bb576bd4cceee1
Samson <chuksdsilent@gmail.com>
2024-11-12 19:26:21 +0100

    initial commit

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -138,7 +138,7 @@ class BookingController extends Controller
      *           {"sanctum": {}},
      *       },
      *      @OA\Parameter(
-     *          name="trx_id"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ",
+     *          name="trx_id                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ",
      *          in="query",
      *          description="trx_id",
      *          required=true,
@@ -156,12 +156,7 @@ class BookingController extends Controller
         if(!auth('sanctum')->check())
             return $utils->message("error","Unauthorized Access." , 401);
 
-        $request->validate([
-            "trx_id" => "required",
-        ]);
-
         $user_id =  auth('sanctum')->user()->id;
-
 
         try {
             return $utils->message("success", "Transaction Completed..." , 200);
@@ -189,7 +184,7 @@ class BookingController extends Controller
      *           {"sanctum": {}},
      *       },
      *     @OA\Parameter(
-     *         name="payment_id"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ",
+     *         name="payment_id                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ",
      *         in="query",
      *         description="2024-04-29 18:00:00",
      *         required=true,
