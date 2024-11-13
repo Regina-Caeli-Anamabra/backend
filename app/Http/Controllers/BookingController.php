16663d34ab6e06bcaad2413d9e925179a0cd4a6e
Samson <chuksdsilent@gmail.com>
2024-11-13 23:45:19 +0100

    initial commit

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -427,7 +427,7 @@ class BookingController extends Controller
      */
     public function store(Request $request, Utils $utils)
     {
-      return  $request->all();
+        $request->all();
         $request->validate([
             "booking_start" => "required",
             "service_id" => "required|int",
