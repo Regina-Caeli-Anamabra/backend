563f3e9ff62228dc53b818354d45140a4eb8c507
Samson <chuksdsilent@gmail.com>
2025-01-22 11:19:44 +0100

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::get('/re-arrange/category', function(){
 });
 
 Route::get('/move-patient-to-users', function(){
+    echo "Starting to move patients";
     \App\Jobs\MovePatientsToUsers::dispatch();
     return 'Data move process started in the background!';
 });
