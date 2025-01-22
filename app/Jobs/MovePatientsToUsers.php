0355b876bd9e28b3426d1d0f96fe7f8f24696199
Samson <chuksdsilent@gmail.com>
2025-01-22 12:13:45 +0100

    initial commit

--- app/Jobs/MovePatientsToUsers.php
+++ app/Jobs/MovePatientsToUsers.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Notification;
 
 class MovePatientsToUsers implements ShouldQueue
