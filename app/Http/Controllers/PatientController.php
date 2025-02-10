da2a352809044105a5553c338c098493d1555190
Samson <chuksdsilent@gmail.com>
2025-02-10 11:53:08 +0100

    initial commit

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -67,7 +67,7 @@ class PatientController extends Controller
 
             if(!auth('sanctum')->check())
                 return $utils->message("error","Unauthorized Access." , 401);
-            $patient = FlutterwavePayment::with("patients")->get();
+            $patient = FlutterwavePayment::with(["patients", "services"])->get();
              PaymentResource::collection($patient);
              $data = [
                  "payments" => PaymentResource::collection($patient),
