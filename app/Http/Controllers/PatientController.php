4be0cc2995df31fcf051ad11d3635d65e7fedbb3
Samson <chuksdsilent@gmail.com>
2025-02-10 01:22:20 +0100

    initial commit

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -69,7 +69,7 @@ class PatientController extends Controller
             if(!auth('sanctum')->check())
                 return $utils->message("error","Unauthorized Access." , 401);
 
-            $payments = DB::table('  flutterwave_payments')
+           return $payments = DB::table('  flutterwave_payments')
                 ->join('services', ' flutterwave_payments.service_id', '=', 'services.id')
                 ->limit(1000)
                 ->orderBy("payments.id", "DESC")
