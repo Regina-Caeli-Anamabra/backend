d6d0743acdd629b873941e1702bd8f899057b906
Samson <chuksdsilent@gmail.com>
2025-02-19 18:18:16 +0100

    initial commit

--- app/utils/Utils.php
+++ app/utils/Utils.php
@@ -86,6 +86,7 @@ class Utils
 
     public function validatePayment($transaction_id)
     {
+        $token = env("FLWSECK");
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => "https://api.flutterwave.com/v3/transactions/". $transaction_id."/verify",
@@ -98,7 +99,7 @@ class Utils
             CURLOPT_CUSTOMREQUEST => "GET",
             CURLOPT_HTTPHEADER => array(
                 "Content-Type: application/json",
-                "Authorization: Bearer " . env("FLWSECK")
+                "Authorization: Bearer $token"
             ),
         ));
 
