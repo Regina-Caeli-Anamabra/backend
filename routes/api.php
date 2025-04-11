084eb694693a7c3522c74354604cd82f63044329
Samson <chuksdsilent@gmail.com>
2025-04-11 03:37:56 +0100

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -78,7 +78,7 @@ Route::group(['prefix' => 'v1/patient', 'middleware' => ['auth:sanctum']], funct
     Route::post('/add-payment', ['App\Http\Controllers\PatientController', 'addPayment']);
     Route::get('/get-users-created', ['App\Http\Controllers\PatientController', 'getAllRegisteredByUser']);
     Route::get('/profile', ['App\Http\Controllers\PatientController', 'profile']);
-    Route::get('/get-details', ['App\Http\Controllers\PatientController', 'getDetails']);
+    Route::post('/get-details', ['App\Http\Controllers\PatientController', 'getDetails']);
     Route::patch('/profile/update', ['App\Http\Controllers\PatientController', 'updateProfile']);
     Route::patch('/inner/password/update', ['App\Http\Controllers\Auth\AuthController', 'innerUpdatePassword']);
     Route::get('/get-categories', ['App\Http\Controllers\BookingController', 'getCategories']);
