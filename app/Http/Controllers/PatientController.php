2fb843f0a4a669b361e8e11dab6852556e419eb9
Samson <chuksdsilent@gmail.com>
2025-04-15 06:41:37 +0100

    changed the service description

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -17,6 +17,7 @@ use App\Models\User;
 use App\Utils\Utils;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
