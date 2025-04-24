b7e6456289036910b906c863a2388f7159c46d1e
Samson <chuksdsilent@gmail.com>
2025-04-24 15:53:10 +0100

    changed the service description

--- python/sync_database.py
+++ python/sync_database.py
@@ -8,7 +8,7 @@ db_config = {
     'host': os.environ.get("DB_HOST"),
     'user': os.environ.get("MYSQL_USER"),
     'password': os.environ.get("MYSQL_PASSWORD"),
-    'database': os.environ.get("DB_DATABASE"),
+    'database': os.environ.get("MYSQL_DATABASE"),
 }
 
 
