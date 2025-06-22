de7750c6141aef3519217c6c5b1901ef542b377c
Samson <chuksdsilent@gmail.com>
2025-06-22 13:36:14 +0100

    initial commit

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -316,7 +316,7 @@ class BookingController extends Controller
             $user_id =  auth('sanctum')->user()->id;
             $identity = $request->get("identity");
 
-            $booking = Bookings::with(["patient:firstName,lastName,phone_no,", "flutterPayment:trx_id","services:service_name"])
+            $booking = Bookings::with(["patient:firstName,lastName,phone_no", "flutterPayment:trx_id","services:service_name"])
                         ->where("identity", $identity)
                         ->firstOrFail();
 
