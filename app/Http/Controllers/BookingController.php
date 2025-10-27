befe24066ec861ef00a2ddff50e086697276e36d
Samson <chuksdsilent@gmail.com>
2025-10-27 05:19:22 +0100

    initial commit

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -778,7 +778,7 @@ class BookingController extends Controller
             if(!auth('sanctum')->check())
                 return $utils->message("error","Unauthorized Access." , 401);
 
-             $user_id =  auth('sanctum')->user()->reg_id;
+             $user_id =  auth('sanctum')->user()->id;
              $service_id = $request->get("service_id");
              $amount = $request->get("amount");
 
