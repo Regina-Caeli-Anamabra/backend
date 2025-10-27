cbdc7eb994eebdccc528ac62f2e174b33fa27d3c
Samson <chuksdsilent@gmail.com>
2025-10-27 16:56:16 +0100

    initial commit

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -92,7 +92,7 @@ class PatientController extends Controller
 
 
         $patient = $request->get("patient_id");
-       return $trx_id = $request->get("trx_id");
+        $trx_id = $request->get("trx_id");
         $payment_id = $request->get("payment_identity");
 
         $user = User::where("reg_id", $patient)->first();
