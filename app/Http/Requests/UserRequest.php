4ba76198863641f833a3f20d6b91638d0186e0bc
Samson <chuksdsilent@gmail.com>
2025-11-09 21:52:32 +0100

    Remove duplicate email check

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -29,7 +29,7 @@ class UserRequest extends FormRequest
             "auth_type" => "required|string",
             "last_name" => "required|string",
             "gender" => "required|string",
-            "email" => "required||max:255|unique:users",
+            "email" => "required|string",
             "marital_status" => "required|string",
             "religion" => "required|string",
             "nationality" => "required|string",
