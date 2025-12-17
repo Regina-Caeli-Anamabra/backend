e498a9203604e5f35df5cecb68fbe755c031c61b
Samson <chuksdsilent@gmail.com>
2025-12-17 21:48:30 +0100

    Added DB Transactions

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -295,42 +295,47 @@ class PatientController extends Controller
 
             $payment =  DB::transaction(function () use ($utils, $patient_id, $request) {
 
-                $oldPatients = PatientDontUse::where("patient_id", $patient_id)->firstOrFail();
-
-                if (substr($oldPatients->phone_no, 0, 1) !== "0") {
-                    $oldPatients->phone_no = "0" . $oldPatients->phone_no;
-                }
                     if (!User::where("reg_id", $patient_id)->where("paid", 1)->exists()) {
 
-                        $user = new User();
-                        $user->reg_id = $patient_id;
-                        $user->phone = $oldPatients->phone_no;
-                        $user->email = $oldPatients->email;
-                        $user->save();
 
-                        $oldPatients->user_id = $user->id;
-                        $oldPatients->save();
+                        if(PatientDontUse::where("patient_id", $patient_id)->exists()){
 
-                        $offlineOnlinePatientSync = new OfflineOnlinePatientsSync();
-                        $offlineOnlinePatientSync->firstName = $oldPatients->firstName;
-                        $offlineOnlinePatientSync->lastName = $oldPatients->glastName;
-                        $offlineOnlinePatientSync->user_id = $user->id;
-                        $offlineOnlinePatientSync->reg_id = $patient_id;
-                        $offlineOnlinePatientSync->phone = $oldPatients->phone_no;
-                        $offlineOnlinePatientSync->date_of_birth = $oldPatients->dateOfBirth;
-                        $offlineOnlinePatientSync->gender = $oldPatients->gender;
-                        $offlineOnlinePatientSync->nature_of_relationship = $oldPatients->next_of_kin_relationship;
-                        $offlineOnlinePatientSync->marital_status = $oldPatients->marital_status;
-                        $offlineOnlinePatientSync->religion = $oldPatients->ethnic;
-                        $offlineOnlinePatientSync->nationality = $oldPatients->nationality;
-                        $offlineOnlinePatientSync->next_of_kin = $oldPatients->next_of_kin;
-                        $offlineOnlinePatientSync->next_of_kin_phone = $oldPatients->next_of_kin_phoneno;
-                        $offlineOnlinePatientSync->state_of_residence = $oldPatients->state_of_residence;
-                        $offlineOnlinePatientSync->address_of_residence = $oldPatients->permanent_address;
-                        $offlineOnlinePatientSync->patient_id = $oldPatients->id;
-                        $offlineOnlinePatientSync->place = "offline";
-                        $offlineOnlinePatientSync->save();
+                            $oldPatients = PatientDontUse::where("patient_id", $patient_id)->firstOrFail();
 
+                            if (substr($oldPatients->phone_no, 0, 1) !== "0") {
+                                $oldPatients->phone_no = "0" . $oldPatients->phone_no;
+                            }
+
+                            $user = new User();
+                            $user->reg_id = $patient_id;
+                            $user->phone = $oldPatients->phone_no;
+                            $user->email = $oldPatients->email;
+                            $user->save();
+
+                            $oldPatients->user_id = $user->id;
+                            $oldPatients->save();
+
+                            $offlineOnlinePatientSync = new OfflineOnlinePatientsSync();
+                            $offlineOnlinePatientSync->firstName = $oldPatients->firstName;
+                            $offlineOnlinePatientSync->lastName = $oldPatients->glastName;
+                            $offlineOnlinePatientSync->user_id = $user->id;
+                            $offlineOnlinePatientSync->reg_id = $patient_id;
+                            $offlineOnlinePatientSync->phone = $oldPatients->phone_no;
+                            $offlineOnlinePatientSync->date_of_birth = $oldPatients->dateOfBirth;
+                            $offlineOnlinePatientSync->gender = $oldPatients->gender;
+                            $offlineOnlinePatientSync->nature_of_relationship = $oldPatients->next_of_kin_relationship;
+                            $offlineOnlinePatientSync->marital_status = $oldPatients->marital_status;
+                            $offlineOnlinePatientSync->religion = $oldPatients->ethnic;
+                            $offlineOnlinePatientSync->nationality = $oldPatients->nationality;
+                            $offlineOnlinePatientSync->next_of_kin = $oldPatients->next_of_kin;
+                            $offlineOnlinePatientSync->next_of_kin_phone = $oldPatients->next_of_kin_phoneno;
+                            $offlineOnlinePatientSync->state_of_residence = $oldPatients->state_of_residence;
+                            $offlineOnlinePatientSync->address_of_residence = $oldPatients->permanent_address;
+                            $offlineOnlinePatientSync->patient_id = $oldPatients->id;
+                            $offlineOnlinePatientSync->place = "offline";
+                            $offlineOnlinePatientSync->save();
+
+                        }
 
                         $trx_id = $utils->generateCode(20);
 
